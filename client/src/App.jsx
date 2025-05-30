8c9deb3697f11feb084385afba06914008152171
rukki20 <rukkiyazainab@gmail.com>
2025-05-30 14:48:42 +0530

    Merge pull request #22 from lakshmi-priya1424/17-basic-layout
    layoutandrouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 
+import AppRoutes from './routes/AppRoutes'
+import Login from './pages/Login'
 import Signup from './pages/Signup'
 
 function App() {
   return (
     <>
-      <Signup/>
+      <AppRoutes/>
     </>
   )
 }

d85bc8850c132a23e2d5402e9077ca6dce396324
lakshmipriya <lakshmipriya7767@gmail.com>
2025-05-31 15:28:24 +0530

    finalcommit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import './App.css'
 import AppRoutes from './routes/AppRoutes'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-import { AuthProvider } from './context/AuthContext'
+import { AuthProvider } from './context/Authcontext'
 import Introd from './pages/dashboard/Introd'
 import { StorageProvider } from '../src/context/StorageContext'
 

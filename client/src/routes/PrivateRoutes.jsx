df52f3889a0fb60c7d1a0369d284d7c43fe8549c
lakshmipriya <lakshmipriya7767@gmail.com>
2025-05-31 16:14:28 +0530

    maincommit

--- client/src/routes/PrivateRoutes.jsx
+++ client/src/routes/PrivateRoutes.jsx
@@ -1,6 +1,10 @@
 import { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router';
+<<<<<<< HEAD
 import AuthContext from '../context/AuthContext';
+=======
+import AuthContext from '../context/Authcontext';
+>>>>>>> 081a667dcb622f73a727b287a8054666b61e600f
 
 export default function PrivateRoute({ children }) {
 	const { isAuth } = useContext(AuthContext);
